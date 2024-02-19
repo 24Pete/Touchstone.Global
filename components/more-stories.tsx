833d6ec24d1104df4825047f11c8fba423931a39
Pistol Pete <peterlawson@24marketing.co.uk>
2024-02-19 12:55:18 +0000

    removed post-preview

--- components/more-stories.tsx
+++ components/more-stories.tsx
@@ -1,4 +1,4 @@
-import PostPreview from "./post-preview";
+
 
 export default function MoreStories({ posts }) {
   return (
@@ -7,17 +7,7 @@ export default function MoreStories({ posts }) {
       Insights
       </h2>
       <div className="grid grid-cols-4 md:grid-cols-4 md:gap-x-4 lg:gap-x-4 gap-y-10 mb-32">
-        {posts.map(({ node }) => (
-          <PostPreview
-            key={node.slug}
-            title={node.title}
-            coverImage={node.featuredImage}
-            date={node.date}
-            author={node.author}
-            slug={node.slug}
-            excerpt={node.excerpt}
-          />
-        ))}
+      
         <button className="bg-azure pt-3 pb-3 pl-6 pr-6 font-bold text-dusk text-lg uppercase text-center"> Load More Insights</button>
       </div>
     </section>
