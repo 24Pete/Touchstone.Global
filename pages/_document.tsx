ccea3a270d10bb9028d2eaaa8a26381bfc487abe
Pistol Pete <peterlawson@24marketing.co.uk>
2024-03-08 09:59:15 +0000

    Adjusted Meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,11 @@ export default function Document() {
       <Head>
       <link rel="stylesheet" href="https://use.typekit.net/cpd3mbf.css"/>
       <link rel="shortcut icon" href="/images/favicon.ico" />
+      <meta name="description" content="Unlocking exponential value with the Touchstone collective" />
+      <meta property="og:image" content="<generated>" />
+<meta property="og:image:type" content="<generated>" />
+<meta property="og:image:width" content="<generated>" />
+<meta property="og:image:height" content="<generated>" />
       </Head>
       <body className="bg-dusk">
   
